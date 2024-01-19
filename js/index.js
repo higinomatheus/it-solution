a66d2e60a58d59a0b9be8a239e5ca95f836e3938
Matheus Higino <matheus.higino@yahoo.com.br>
2024-01-19 11:33:35 -0300

    Finalização

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
 const currentYearSpan = document.querySelector("#current-year");
 currentYearSpan.innerHTML = new Date().getFullYear();
-console.log(currentYearSpan);
+
+// Função para animação de rolar para a seção alvo
+function scrollToTarget(hash) {
+    const targetElement = document.querySelector(hash);
+    const targetOffsetTop = targetElement.offsetTop - 100;
+  
+    window.scrollTo({
+      top: targetOffsetTop,
+      behavior: 'smooth'
+    });
+  }
+  
+  // Adiciona o evento de clique aos elementos do menu e botões
+  document.querySelectorAll("#navbar a, .btn").forEach(function (element) {
+    element.addEventListener("click", function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        scrollToTarget(this.hash);
+      }
+    });
+  });
+  
+  // Adiciona o evento de rolagem à janela
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector("#navbar");
+    
+    if (window.scrollY > 150) {
+      navbar.style.opacity = 0.9;
+    } else {
+      navbar.style.opacity = 1;
+    }
+  });  
